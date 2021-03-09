f3b2c78daaf1e8905967baba44c64faf02db6b3e
Brian Goff <cpuguy83@gmail.com>
2021-03-09 19:23:16 +0000

    Output frozen iamges as artifact for engine tests

--- engine/20.10/build.sh
+++ engine/20.10/build.sh
@@ -3,7 +3,7 @@
 set -e -o xtrace
 
 : ${VERSION:=dev}
-: ${OUTPUT:="$(pwd)/bin"}
+: ${OUTPUT:="$(pwd)"}
 export OUTPUT
 
 : ${GOPATH:=~/go}
@@ -22,13 +22,17 @@ TINI_COMMIT="$(. ${GOPATH}/src/github.com/docker/docker/hack/dockerfile/install/
 "}
 : ${BUILDMODE:="pie"}
 
-go build -o "${OUTPUT}/dockerd" -tags "${BUILDTAGS}" -ldflags "${LDFLAGS}" -buildmode "${BUILDMODE}" ${EXTRA_BUILD_FLAGS} github.com/docker/docker/cmd/dockerd
+go build -o "${OUTPUT}/bin/dockerd" -tags "${BUILDTAGS}" -ldflags "${LDFLAGS}" -buildmode "${BUILDMODE}" ${EXTRA_BUILD_FLAGS} github.com/docker/docker/cmd/dockerd
 
-(
-	cd "${GOPATH}/src/github.com/docker/docker"
-	export PREFIX="${OUTPUT}"
-	hack/dockerfile/install/install.sh proxy
-	hack/dockerfile/install/install.sh tini
-	hack/dockerfile/install/install.sh gotestsum # Testing dependency
-	hack/make.sh build-integration-test-binary
-)
+export PREFIX="${OUTPUT}/bin/"
+hack/dockerfile/install/install.sh proxy
+hack/dockerfile/install/install.sh tini
+hack/make.sh build-integration-test-binary
+hack/dockerfile/install/install.sh gotestsum # Testing dependency
+contrib/download-frozen-image-v2.sh "${OUTPUT}/frozen" \
+	buildpack-deps:buster@sha256:d0abb4b1e5c664828b93e8b6ac84d10bce45ee469999bef88304be04a2709491 \
+	busybox:latest@sha256:95cf004f559831017cdf4628aaf1bb30133677be8702a8c5f2994629f637a209 \
+	busybox:glibc@sha256:1f81263701cddf6402afe9f33fca0266d9fff379e59b1748f33d3072da71ee85 \
+	debian:bullseye@sha256:7190e972ab16aefea4d758ebe42a293f4e5c5be63595f4d03a5b9bf6839a4344 \
+	hello-world:latest@sha256:d58e752213a51785838f9eed2b7a498ffa1cb3aa7f946dda11af39286c3db9a9 \
+	arm32v7/hello-world:latest@sha256:50b8560ad574c779908da71f7ce370c0a2471c098d44d1c8f6b513c5a55eeeb1
