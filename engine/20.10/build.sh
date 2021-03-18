1060adf09966791449912de1ef335979de9424a1
Brian Goff <cpuguy83@gmail.com>
2021-03-18 23:32:44 +0000

    Make sure we set git commit whe building engine
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- engine/20.10/build.sh
+++ engine/20.10/build.sh
@@ -7,10 +7,12 @@ set -e -o xtrace
 : ${GOPATH:=~/go}
 TINI_COMMIT="$(. ${GOPATH}/src/github.com/docker/docker/hack/dockerfile/install/tini.installer; echo ${TINI_COMMIT})"
 
+: ${ENGINE_COMMIT:="$(git rev-parse HEAD || echo dev)"}
+
 : ${BUILDTAGS:="journald apparmor seccomp"}
 : ${LDFLAGS:=" \
 	-X github.com/docker/docker/dockerversion.Version=${VERSION} \
-	-X github.com/docker/docker/dockerversion.GitCommit=${GITCOMMIT} \
+	-X github.com/docker/docker/dockerversion.GitCommit=${ENGINE_COMMIT} \
 	-X github.com/docker/docker/dockerversion.BuildTime=${BUILDTIME} \
 	-X github.com/docker/docker/dockerversion.IAmStatic=false \
 	-X github.com/docker/docker/dockerversion.PlatformName=${PLATFORM} \
